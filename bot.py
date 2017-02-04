bb265d4d7672ca56c2a412ec75a9f365a7143f5d
John Miller <john@johngm.com>
2017-02-03 21:51:31 -0800

    initial commit

---
+++ bot.py
@@ -0,0 +1,14 @@
+import commentjson
+
+from pygeneralsio import Client
+
+
+CONFIG_FILENAME = 'config.json'
+
+def main():
+    config = commentjson.loads(open(CONFIG_FILENAME).read())
+    client = Client(config['server'], config['user_id'], config['username'])
+
+
+if __name__ == '__main__':
+    main()
